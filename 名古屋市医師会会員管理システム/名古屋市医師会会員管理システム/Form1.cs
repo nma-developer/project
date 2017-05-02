f0fd4a52e1db42c05d382db8fa99b413d13b4673
医師会 名古屋市 <nma_developer@outlook.jp>
2017-05-02 09:59:47 +0900

    新規

---
+++ 名古屋市医師会会員管理システム/名古屋市医師会会員管理システム/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 名古屋市医師会会員管理システム
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
